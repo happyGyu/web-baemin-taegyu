fe453ca8d1d18804f8cb9ba4b9abe99850040497
happyGyu <happyboyto.dev@gmail.com>
2022-07-06 22:14:28 +0900

    feat: markup agree page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const joinRouter = require("./routers/joinRouter");
 const pageRouter = require("./routers/pageRouter");
 const port = 3000;
 const app = express();
@@ -8,6 +9,7 @@ app.use("/style", express.static(path.join(__dirname, "public", "style")));
 app.use("/js", express.static(path.join(__dirname, "public", "js")));
 
 app.set("view engine", "pug");
+app.use("/join", joinRouter);
 app.use("/", pageRouter);
 
 app.listen(port, () => {
