33d86968bbae08ddb17de5e2c60fce1bd4c45b85
woowahan <woowahan@woowahan-MacBookPro-106.local>
2022-07-06 12:55:31 +0900

    feat: markup mainpage

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require("express");
+const path = require("path");
 const pageRouter = require("./routers/pageRouter");
 const port = 3000;
 const app = express();
+
+app.use("/style", express.static(path.join(__dirname, "public/style")));
+app.use("/js", express.static(path.join(__dirname, "public/js")));
 
 app.set("view engine", "pug");
 app.use("/", pageRouter);
--- routers/pageRouter.js
+++ routers/pageRouter.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (_, res) => {
-  res.render("main");
+  res.render("main", { title: "테스트 타이틀" });
 });
 
 module.exports = router;
