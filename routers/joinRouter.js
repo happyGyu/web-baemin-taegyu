3d4fbbf836e05e547b7dcdcf5c001ef107de82eb
happyGyu <happyboyto.dev@gmail.com>
2022-07-07 10:42:31 +0900

    feat: markup phone number inputbox

--- routers/joinRouter.js
+++ routers/joinRouter.js
@@ -5,4 +5,8 @@ router.get("/agree", (_, res) => {
   res.render("agree", { title: "회원가입 | 약관동의" });
 });
 
+router.get("/auth", (_, res) => {
+  res.render("auth", { title: "회원가입 | 인증" });
+});
+
 module.exports = router;
