cc2df628cda66ae47960b5c498d43d4c13db8dbc
happyGyu <happyboyto.dev@gmail.com>
2022-07-10 18:31:50 +0900

    fix: 로그인페이지 에러 처리 로직 수정
    에러메시지 출력 후 사용자가 값을 다시 입력할 때도 남아있던 에러를
    사라지도록 수정함. 또한 input 아래에 빨간색 선을 추가해 가시성 향상.

--- public/js/login.js
+++ public/js/login.js
@@ -1,42 +1,49 @@
 import { $, addOrRemoveClass } from "./domUtil.js";
 
-const handleLoginSubmit = (
-  $idInput,
-  $pwInput,
-  $idInputAlertMessage,
-  $pwInputAlertMessage,
-  event
-) => {
+const init = () => {
+  const $loginForm = $(".login-form");
+  const $idInput = $(".login-input__id");
+  const $pwInput = $(".login-input__pw");
+
+  $loginForm.addEventListener(
+    "submit",
+    handleLoginSubmit.bind(null, $idInput, $pwInput)
+  );
+
+  $loginForm.addEventListener("input", handleFormInput);
+};
+
+const handleLoginSubmit = ($idInput, $pwInput, event) => {
   const isIdValid = checkInputValidity($idInput);
   const isPwValid = checkInputValidity($pwInput);
   if (!isIdValid || !isPwValid) {
     event.preventDefault();
   }
-  addOrRemoveClass($idInputAlertMessage, "hidden", isIdValid);
-  addOrRemoveClass($pwInputAlertMessage, "hidden", isPwValid);
+  handleInputError($idInput, !isIdValid);
+  handleInputError($pwInput, !isPwValid);
 };
 
 const checkInputValidity = (input) => {
   return input.value.length > 0;
 };
 
-const init = () => {
-  const $loginForm = $(".login-form");
-  const $idInput = $(".login-input__id");
-  const $pwInput = $(".login-input__pw");
-  const $idInputAlertMessage = $idInput.nextElementSibling;
-  const $pwInputAlertMessage = $pwInput.nextElementSibling;
+const handleInputError = ($input, isError) => {
+  const $alertMessage = getInputAlertMessageDOM($input);
+  addOrRemoveClass($alertMessage, "hidden", !isError);
+  addOrRemoveClass($input, "login-input--error", isError);
+};
 
-  $loginForm.addEventListener(
-    "submit",
-    handleLoginSubmit.bind(
-      null,
-      $idInput,
-      $pwInput,
-      $idInputAlertMessage,
-      $pwInputAlertMessage
-    )
-  );
+const getInputAlertMessageDOM = ($input) => {
+  const $alertMessage = $input
+    .closest(".input-container")
+    .querySelector(".input-alert-message");
+  return $alertMessage;
+};
+
+const handleFormInput = ({ target }) => {
+  const $alertMessage = getInputAlertMessageDOM(target);
+  addOrRemoveClass($alertMessage, "hidden", true);
+  addOrRemoveClass(target, "login-input--error", false);
 };
 
 init();
