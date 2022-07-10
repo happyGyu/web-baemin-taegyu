a2b2758bb43d15a53c4001e1c926ddef9ab74085
happyGyu <happyboyto.dev@gmail.com>
2022-07-11 01:33:38 +0900

    refactor: userinfo handler 함수 리팩토링

--- public/js/constant.js
+++ public/js/constant.js
@@ -1,3 +1,10 @@
 export const MAX_PHONE_NUN_LENGTH = 11;
 export const MIN_PHONE_NUN_LENGTH = 11;
 export const CERTIFICATION_NUM_LENGTH = 4;
+
+export const MIN_PW_LENGTH = 10;
+export const MIN_PW_ELEMENT_TYPE = 2;
+
+export const PW_SERIAL_NUM_LIMIT = 3;
+export const BIRTHDAY_NUM_LENGTH = 8;
+export const FORMATTED_BIRTHDAY_LENGTH = 10;
